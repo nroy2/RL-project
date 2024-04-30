ca01f9b72f38ec203c4554ae64497dc997550b5d
kuroni <bill081001@gmail.com>
2024-04-30 11:46:56 -0500

    Add legend to dotted line

--- test.py
+++ test.py
@@ -20,7 +20,7 @@ def plot_test_rewards(test_name, distribution, num_items, num_episodes, evaluate
 
     # Calculate expected max, which is the expected upper limit of how well any algorithm can do
     expected_max = integrate.quad(lambda x : 1 - distribution.cdf(x) ** num_items, 0, np.inf)[0]
-    plt.axhline(expected_max, linestyle='--')
+    plt.axhline(expected_max, linestyle='--', label='Expected prophet reward')
 
     for file in sorted(os.listdir('reward_file')):
         if file.startswith(f"{test_name}_"):
@@ -58,7 +58,7 @@ def train_and_compare_models(test_name, distribution, num_items, num_episodes, e
     agents = [
         # DQN(env, name='DQN'),
         # SarsaLambdaAgent(env=env, name='SarsaLambdaAgent', gamma=1, lam=0.2, alpha=0.05, X=tile_coding),
-        REINFORCEAgent(env=env, name='REINFORCENoBaseline', gamma=1, pi=pi_no_baseline, V=Baseline(0.)),
+        # REINFORCEAgent(env=env, name='REINFORCENoBaseline', gamma=1, pi=pi_no_baseline, V=Baseline(0.)),
         # MedianMaxThreshold(env, name='MedianMaxThreshold'),
         # OCRSBased(env, name='OCRSBased'),
         # SingleSampleMaxThreshold(env, name='SingleSampleMaxThreshold'),
@@ -77,51 +77,51 @@ def train_and_compare_models(test_name, distribution, num_items, num_episodes, e
     plot_test_rewards(test_name, distribution, num_items, num_episodes, evaluate_interval)
 
 ### Training
-
-# train_and_compare_models(
-#     test_name='Expon-15-1k-10-100',
-#     distribution=stats.expon(loc=0, scale=100),
-#     num_items=15,
-#     num_episodes=1000,
-#     evaluate_interval=10,
-#     samples_per_eval=100
-# )
-
-# Uniform
-# train_and_compare_models(
-#     test_name='Uniform-15-1k-10-100',
-#     distribution=stats.uniform(loc=0, scale=100),
-#     num_items=15,
-#     num_episodes=1000,
-#     evaluate_interval=10,
-#     samples_per_eval=100
-# )
-
-# Half Normal
-# train_and_compare_models(
-#     test_name='Halfnorm-15-1k-10-100',
-#     distribution=stats.halfnorm(loc=0, scale=100),
-#     num_items=15,
-#     num_episodes=1000,
-#     evaluate_interval=10,
-#     samples_per_eval=100
-# )
-
-### Loading and evaluating
-distribution, num_items = stats.expon(loc=0, scale=100), 15
-env = ProphetInequalityEnv(distribution=distribution, num_items=num_items)
-
-# Tile coding for SARSA Agent
-reward_low, reward_high = distribution.interval(confidence=0.999)
-tile_coding = StateActionFeatureVectorWithTile(
-    state_low=np.array([0, reward_low]),
-    state_high=np.array([num_items + 1, reward_high]),
-    num_actions=2,
-    num_tilings=1,
-    tile_width=np.array([1, (reward_high - reward_low) / 100])
+# Exponential
+train_and_compare_models(
+    test_name='Expon-15-1m-10k-10k',
+    distribution=stats.expon(loc=0, scale=100),
+    num_items=15,
+    num_episodes=1_000_000,
+    evaluate_interval=10_000,
+    samples_per_eval=10_000
 )
 
-model = SarsaLambdaAgent(env=env, name='SarsaLambdaAgent', gamma=1, lam=0.2, alpha=0.05, X=tile_coding)
-model.load_model('model_file/Expon-15-1m-10k-10k_SarsaLambdaAgent')
+# Uniform
+train_and_compare_models(
+    test_name='Uniform-15-1m-10k-10k',
+    distribution=stats.uniform(loc=0, scale=100),
+    num_items=15,
+    num_episodes=1_000_000,
+    evaluate_interval=10_000,
+    samples_per_eval=10_000
+)
 
-print(evaluate_agent(env, model, 10000))
+# Half Normal
+train_and_compare_models(
+    test_name='Halfnorm-15-1m-10k-10k',
+    distribution=stats.halfnorm(loc=0, scale=100),
+    num_items=15,
+    num_episodes=1_000_000,
+    evaluate_interval=10_000,
+    samples_per_eval=10_000
+)
+
+### Loading and evaluating
+# distribution, num_items = stats.expon(loc=0, scale=100), 15
+# env = ProphetInequalityEnv(distribution=distribution, num_items=num_items)
+
+# # Tile coding for SARSA Agent
+# reward_low, reward_high = distribution.interval(confidence=0.999)
+# tile_coding = StateActionFeatureVectorWithTile(
+#     state_low=np.array([0, reward_low]),
+#     state_high=np.array([num_items + 1, reward_high]),
+#     num_actions=2,
+#     num_tilings=1,
+#     tile_width=np.array([1, (reward_high - reward_low) / 100])
+# )
+
+# model = SarsaLambdaAgent(env=env, name='SarsaLambdaAgent', gamma=1, lam=0.2, alpha=0.05, X=tile_coding)
+# model.load_model('model_file/Expon-15-1m-10k-10k_SarsaLambdaAgent')
+
+# print(evaluate_agent(env, model, 10000))
